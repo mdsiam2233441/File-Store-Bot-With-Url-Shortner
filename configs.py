0467b77e9bbd12ec1a10d1b8e09852308982901b
MD Siam Sarkar <109170937+mdsiam223344@users.noreply.github.com>
2024-06-20 01:18:12 -0700

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,19 +3,19 @@ import os
 class Config(object):
   API_ID = int(os.environ.get("API_ID", "24017397"))
   API_HASH = os.environ.get("API_HASH", "0f2fe9bc1733e762a3ceef7c1372ce40")
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "6243663333:AAHSCVPrie0kqTr1ZdW7rnwNhi0ctwYurEQ")
   BOT_USERNAME = os.environ.get("BOT_USERNAME", "Ottwebdl1bot")
   DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002246272311"))
-  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "coffelink.online/")
-  SHORTLINK_API = os.environ.get('SHORTLINK_API', "2134e64033fb0e9be820e4158ea151159e7f3375")
+  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "instantearn.in")
+  SHORTLINK_API = os.environ.get('SHORTLINK_API', "fe76f8810ff923d8a6aff21cc743153a2a958c1e")
   BOT_OWNER = int(os.environ.get("BOT_OWNER", "1686274364"))
-  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://<sadrayispi>:<Tof12oBkLtKVdjnp>@siam.thsybi2.mongodb.net/")
+  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://<sadrayispi>:<Tof12oBkLtKVdjnp>siam.thsybi2.mongodb.net/")
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002246272311")
   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002246272311"))
-  BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
+  BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "-1002246272311").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
-  BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
+  BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1002246272311").split()))
   OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
   ABOUT_BOT_TEXT = f"""
 This is a Permanent FileStore Bot. 
@@ -32,7 +32,7 @@ Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edi
 ╰──────[ 😎 ]───────────⍟
 """
   ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [VJ](https://telegram.me/KingVj01)
+🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [VJ](https://t.me/skrss2233)
  
  I am Super noob Please Support My Hard Work.
 
